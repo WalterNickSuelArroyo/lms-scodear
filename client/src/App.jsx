fd1e2552aef3b63a7a3cfb3dbd55b1fca06e2bf9
WalterNickSuelArroyo <nicodehero21@outlook.com>
2025-01-08 11:44:35 -0500

    Build Dashboard Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import MainLayout from "./layout/MainLayout";
 import Courses from "./pages/students/Courses";
 import MyLearning from "./pages/students/MyLearning";
 import Profile from "./pages/students/Profile";
+import Sidebar from "./pages/admin/Sidebar";
+import Dashboard from "./pages/admin/Dashboard";
+import CourseTable from "./pages/admin/course/CourseTable";
 
 const appRouter = createBrowserRouter([
   {
@@ -36,6 +39,43 @@ const appRouter = createBrowserRouter([
       {
         path: "profile",
         element: <Profile />
+      },
+
+      // admin routes start from here
+      {
+        path: "admin",
+        element: (
+          <Sidebar />
+          // <AdminRoute>
+          //   <Sidebar />
+          // </AdminRoute>
+        ),
+        children: [
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "course",
+            element: <CourseTable />,
+          },
+          // {
+          //   path: "course/create",
+          //   element: <AddCourse />,
+          // },
+          // {
+          //   path: "course/:courseId",
+          //   element: <EditCourse />,
+          // },
+          // {
+          //   path: "course/:courseId/lecture",
+          //   element: <CreateLecture />,
+          // },
+          // {
+          //   path: "course/:courseId/lecture/:lectureId",
+          //   element: <EditLecture />,
+          // },
+        ],
       },
     ],
   },
