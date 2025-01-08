f8a0ec02fb67cf1d4e187857a21c0fb06b50ad04
WalterNickSuelArroyo <nicodehero21@outlook.com>
2025-01-08 02:58:58 -0500

    setup react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,41 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import HeroSection from './pages/students/HeroSection'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import HeroSection from "./pages/students/HeroSection";
+import MainLayout from "./layout/MainLayout";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <HeroSection />
+            {/* Course */}
+          </>
+        ),
+      },
+      {
+        path: "login",
+        element: (
+          <>
+            <Login />
+          </>
+        ),
+      },
+    ],
+  },
+]);
 
 function App() {
-
   return (
     <main>
-      <Navbar />
-      <HeroSection />
-      <Login />
+      <RouterProvider router={appRouter} />
     </main>
-    
-  )
+  );
 }
 
-export default App
+export default App;
